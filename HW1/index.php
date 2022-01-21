be1722687ba9d2cdfcf4f868e58a648c4683a9cb
SergeiArtFl <94012680+SergeiArtFl@users.noreply.github.com>
2022-01-21 11:06:50 +0300

    Add files via upload

---
+++ HW1/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DocumentPhp</title>
+</head>
+<body>
+    <?php
+        // phpinfo();
+        // Задание №1
+            echo '<strong>Задание №1</strong>';
+            $a = 10;
+            $b = 2;
+            $sum1 = $a + $b;
+            $razn1 = $a - $b;
+            $proizv1 = $a * $b;
+            $chastn1 = $a / $b;
+            echo "<br/>Сумма a и b равна: {$sum1}. <br/>Разность a и b равна: {$razn1}. <br/>Произведение a и b равно: {$proizv1}. <br/>Частное a и b равно: {$chastn1}.";
+            echo '<hr />';
+        // Задание №2
+            echo '<strong>Задание №2</strong>';
+            $x = 2;
+            $y = 6;
+            $z = 9;
+            $result2 = ($x + 1)*4 - 2*($z - 2*pow($x, 2) + pow($y, 2));
+            echo "<br/>Результат выражения равен: {$result2}.";
+            echo '<hr />';
+        // Задание №3
+            echo '<strong>Задание №3</strong>';
+            $a = 17;
+            $b = 10;
+            $c = $a - $b;
+            $d = 7;
+            $result = $c + $d;
+            echo "<br/>Результат равен: {$result}.";
+            echo '<hr />';
+        // Задание №4
+             echo '<strong>Задание №4</strong>';
+             $text1 = 'Привет,';
+             $text2 = ' Мир!';
+             echo "<br/>{$text1}{$text2}";
+             echo '<hr />';
+        // Задание №5
+             echo '<strong>Задание №5</strong>';
+             $name = 'Сергей';
+             echo "<br/>Привет, {$name}!";
+             echo '<hr />';
+        // Задание №6
+             echo '<strong>Задание №6</strong>';
+             $num = '12345';
+             $a6 = $num[0];
+             $b6 = $num[1];
+             $c6 = $num[2];
+             $d6 = $num[3];
+             $e6 = $num[4];
+             $sum6 = $num[0] + $num[1] + $num[2] + $num[3] + $num[4];
+             echo "<br/>Сумма цифр числа {$num} равна: {$sum6}.";
+             echo '<hr />';
+        // Задание №7
+             echo '<strong>Задание №7</strong>';
+             $today = getdate();
+             echo '<br/>';
+             print_r($today);
+             $secondsMinutes = $today[minutes] * 60;
+             $secondsHours = $today[hours] * 3600;
+             $secondsMDay = $today[mday] * 24 * 3600;
+             $NYear = $today[year] - 2000;
+             $secondsYear = ($NYear * 365 + intdiv($NYear, 4)) * 24 * 3600;
+             $sumSeconds = $secondsYear + $secondsMDay + $secondsHours + $secondsMinutes + $today[seconds];
+             echo "<br/>С начала 2000 года прошло {$sumSeconds} секунд.";
+             echo '<hr />';
+         // Задание №8
+             echo '<strong>Задание №8</strong>';
+             $hour = $today[hours];
+             $minut = $today[minutes];
+             $second = $today[seconds];
+             echo 'Текущее время: ' . $hour . ':' . $minut . ':' . $second ;
+             echo '<hr />';
+        // Задание №9
+             echo '<strong>Задание №9</strong>';
+             $var = 1;
+             $var += 12;
+             $var -= 14;
+             $var *= 5;
+             $var /= 14;
+             $var++;
+             $var--;
+             echo '<br/>';
+             echo $var;
+             echo '<hr />';
+        // Задание №10 работат только на латинице
+             echo '<strong>Задание №10</strong>';
+             define('Surname', 'Artsiukh');
+             $name = 'Sergei';
+             $name2 = 'Vladimirovich';
+             $age = 29;
+             $name11 = $name[0];
+             $name21 = $name2[0];
+             if (defined('Surname')){
+                echo "<br/>Меня зовут " . Surname . " {$name11}. {$name21}.<br/>Мне {$age} лет";
+             };
+             echo '<hr />';
+    ?>
+</body>
+</html>
